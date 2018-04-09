21b5d525a822dcb97ad8122749c6a67133853c18
Clément Correia <clement.correia@avizzeo.com>
2018-04-09 16:44:49 +0200

    #2 personnalisation du formulaire de connexion

--- src/CLMBundle/Controller/DefaultController.php
+++ src/CLMBundle/Controller/DefaultController.php
@@ -8,6 +8,8 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CLMBundle:Default:index.html.twig');
+        return $this->render('CLMBundle:Default:index.html.twig', array(
+
+        ));
     }
 }
